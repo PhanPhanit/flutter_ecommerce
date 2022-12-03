7efbd76775c8a2b1e12ba536425e5cfca1688b75
Phan Phanit <phanphanit@Phans-MacBook-Pro.local>
2022-12-03 20:11:25 +0700

    first commit

---
+++ lib/ui/splash_screen.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_ecommerce/const/AppColors.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: AppColors.deep_orange,
+    );
+  }
+}
